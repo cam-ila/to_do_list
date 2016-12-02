68fad733716c3c64f6eaad772526afb553eb64aa
Bignone Camila <camilabignone@gmail.com>
2016-12-01 23:21:25 -0300

    url routes

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,12 @@
 class List < ApplicationRecord
-	validates :name, presence: true, uniqueness: true, length: { minimum: 2 }
+	validates :name, presence: true, length: { minimum: 2 }
+
 	validates :url, presence: true, uniqueness: true
 
 	has_many :task, dependent: :destroy
 	validates_associated :task
+	#ver que el nombre no sea unico
+	def to_param
+		url
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-	resources :lists
   #root to: 'lists#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'lists#new'
-  # get '/:url', to: 'lists#show'
-
+ # get '/:url', to: 'lists#show', id: "lists#show"
+  
   resources :lists
   resources :simple_tasks
   resources :long_tasks
   resources :temporary_tasks
+	get '/:id', to: 'lists#show'
 end
